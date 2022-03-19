9ee3ad29a3c6c380c0e60c1d51abfc9635ccd32c
Rahul jangir <84994136+OICjangirrahul@users.noreply.github.com>
2022-03-19 17:06:07 +0900

    save

--- admin/post.php
+++ admin/post.php
@@ -59,7 +59,7 @@
                               <td><?php echo $row['category_name'];?></td>
                               <td><?php echo $row['post_date'];?></td>
                               <td><?php echo $row['username'];?></td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
+                              <td class='edit'><a href='update-post.php?id=<?php echo $row['post_id']?>'><i class='fa fa-edit'></i></a></td>
                               <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                      <?php }?>
